683bcfef26fd1c7a531df81f6911735383df3787
Jody Niko <jody.kohlbacher@gmail.com>
2026-06-05 03:04:01 +0200

    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
+
+export const isProd = process.env.NODE_ENV === 'production';
+module.exports = { 
+  basePath: isProd ? '/nukipizookie.gay' : '', 
+  assetPrefix: isProd ? '/nukipizookie.gay/' : '', 
+  images: { unoptimized: true, }, 
+};
+
+// export const getPath = (path: string): string => `${process.env.NEXT_PUBLIC_BASE_PATH || ''}${path}`;
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
 };
 
 export default nextConfig;
