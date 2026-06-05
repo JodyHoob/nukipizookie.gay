ee5822e6452156c9038b387562f2c0c1c3f8067f
Jody Niko <jody.kohlbacher@gmail.com>
2026-06-05 03:05:19 +0200

    .

--- next.config.ts
+++ next.config.ts
@@ -6,21 +6,39 @@
 
 // export default nextConfig;
 
+// import type { NextConfig } from "next";
+
+// export const isProd = process.env.NODE_ENV === 'production';
+// module.exports = { 
+//   basePath: isProd ? '/nukipizookie.gay' : '', 
+//   assetPrefix: isProd ? '/nukipizookie.gay/' : '', 
+//   images: { unoptimized: true, }, 
+// };
+
+// // export const getPath = (path: string): string => `${process.env.NEXT_PUBLIC_BASE_PATH || ''}${path}`;
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   experimental: {
+//     optimizePackageImports: ["@chakra-ui/react"],
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
-export const isProd = process.env.NODE_ENV === 'production';
-module.exports = { 
-  basePath: isProd ? '/nukipizookie.gay' : '', 
-  assetPrefix: isProd ? '/nukipizookie.gay/' : '', 
-  images: { unoptimized: true, }, 
-};
-
-// export const getPath = (path: string): string => `${process.env.NEXT_PUBLIC_BASE_PATH || ''}${path}`;
+const repoName = "nukipizookie.gay";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    optimizePackageImports: ["@chakra-ui/react"],
+  output: "export",
+
+  basePath: process.env.NODE_ENV === "production" ? `/${repoName}` : "",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? `/${repoName}/` : "",
+
+  images: {
+    unoptimized: true,
   },
 };
 
