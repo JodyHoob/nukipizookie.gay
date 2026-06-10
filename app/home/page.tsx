48213708b1d0f4a55f2afedc716dad35e4bc2652
Jody Niko <jody.kohlbacher@gmail.com>
2026-06-10 23:48:32 +0200

    Homo

---
+++ app/home/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { Container, Box, Heading, Center, Image, VStack, HStack, Tabs, Separator, Stack } from "@chakra-ui/react";
+import { Epilogue } from "next/font/google"
+
+const epilogue = Epilogue({ 
+  weight: "700"
+});
+
+export default function Homo() {
+  return (
+    <Container maxW="full" px={0}>
+        <Box fontSize={20}>
+            
+        </Box>
+        
+    </Container>
+  );
+}
